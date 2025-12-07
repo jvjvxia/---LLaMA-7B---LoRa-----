6fc54a50c54a680c345fc02679087bda4677bbd5
jvjvxia <1298445613@qq.com>
2025-12-07 18:01:09 +0800

    Create infer.sh

---
+++ scripts/infer.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# If inferring with the llama model, set 'use_lora' to 'False' and 'prompt_template' to 'ori_template'.
+# If inferring with the default alpaca model, set 'use_lora' to 'True', 'lora_weights' to 'tloen/alpaca-lora-7b', and 'prompt_template' to 'alpaca'.
+# If inferring with the llama-med model, download the LORA weights and set 'lora_weights' to './lora-llama-med' (or the exact directory of LORA weights) and 'prompt_template' to 'med_template'.
+
+python infer.py \
+    --base_model '/root/autodl-tmp/huggyllama/llama-7b' \
+    --lora_weights '/tmp/pycharm_project_444/lora-llama-med-e1' \
+    --use_lora True \
+    --instruct_dir './data/infer.json' \
+    --prompt_template 'med_template'
